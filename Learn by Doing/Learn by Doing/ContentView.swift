b59d47492e69d2f7318489d4ea822824bcfb4e1f
Sheep1sik <bohg6799@naver.com>
2024-03-11 17:06:15 +0900

    Feat: Setup

---
+++ Learn by Doing/Learn by Doing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by 양원식 on 2024/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by 양원식 on 2024/03/11.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
