8a8bc32797dbb703e80dcab746575fa87fd0c727
Sheep1sik <bohg6799@naver.com>
2024-03-12 14:06:02 +0900

    Feat: Animation, Haptic Feedback, Alert window, Sticker 적용

--- Learn by Doing/Learn by Doing/CardView.swift
+++ Learn by Doing/Learn by Doing/CardView.swift
@@ -12,6 +12,13 @@ struct CardView: View {
     
     var card: Card
     
+    @State private var fadeIn: Bool = false
+    @State private var moveDownward: Bool = false
+    @State private var moveUpward: Bool = false
+    @State private var showAlert: Bool = false
+    
+    var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
+    
     // MARK: - FUNCTION
     
     // MARK: - BODY
@@ -19,6 +26,7 @@ struct CardView: View {
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
             
             VStack(content: {
                 Text(card.title)
@@ -31,12 +39,13 @@ struct CardView: View {
                     .foregroundColor(Color.white)
                     .italic()
             })
-            .offset(y: -218)
+            .offset(y: moveDownward ? -218 : -300 )
             
             Button(action: {
                 print("Button was tapped.")
-                
                 playSound(sound: "sound-chime", type: "mp3")
+                self.hapticImpact.impactOccurred()
+                self.showAlert.toggle()
             }) {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -53,12 +62,24 @@ struct CardView: View {
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
-            .offset(y: 210)
+            .offset(y: moveUpward ? 210 : 300)
         }
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear() {
+            withAnimation(.linear(duration: 1.2)) {
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)) {
+                self.moveDownward.toggle()
+                self.moveUpward.toggle()
+            }
+        }
+        .alert(isPresented: $showAlert, content: {
+            Alert(title: Text(card.title), message: Text(card.message), dismissButton: .default(Text("OK")))
+        })
     }
 }
 
